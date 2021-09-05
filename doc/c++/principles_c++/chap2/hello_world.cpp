f61b041a698aeeaf33a3b3ea13d630f084baea08
rockyhw <hongchuan91@gmail.com>
2021-09-05 10:14:24 -0600

    start chap3

---
+++ doc/c++/principles_c++/chap2/hello_world.cpp
@@ -0,0 +1,6 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+	cout << "hello, world\n";
+	return 0;
+}
---
+++ doc/c++/principles_c++/chap3/name.cpp
@@ -0,0 +1,9 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+	cout << "enter your first name\n";
+	string first_name;
+	cin >> first_name;
+	cout << "hello," << first_name << "\n";
+	return 0;
+}
